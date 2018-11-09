8e828d801974ab3a718e483a1c2b26d9358db40c
laimamssd <35742714+laimamssd@users.noreply.github.com>
2018-11-09 20:46:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 const baktash = new Discord.Client();
 const prefix = "1";
 
-Baktash.on("ready", async ready => {
+baktash.on("ready", async ready => {
     setInterval(function() {
-        Baktash.channels.get("510523394102657039").send("**Baktash Credits Bot.**");
+        baktash.channels.get("510523394102657039").send("**Baktash Credits Bot.**");
     }, 25);
 });
 setTimeout(function() {
     cya.destroy();
 }, 360000);
 
-Baktash.on("message", message => {
+baktash.on("message", message => {
     if(message.content.startsWith(prefix + "say")) {
             if(message.author.id !== "331081268731052042");
         let args = message.content.split(" ").slice(1).join(" ");
