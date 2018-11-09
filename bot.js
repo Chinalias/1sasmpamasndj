2dfd182d1c93b61d7bd732f10fa2d87ce5621669
laimamssd <35742714+laimamssd@users.noreply.github.com>
2018-11-09 20:53:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const baktash = new Discord.Client();
-const prefix = "1";
+const prefix = "$";
 
 baktash.on("ready", async ready => {
     setInterval(function() {
